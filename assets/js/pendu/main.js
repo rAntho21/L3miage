f13dc005148a57e29fa2c240046dc27a8de46c68
Carni-Lagarde Jayson <jayson.carni-lagarde@etu.univ-cotedazur.fr>
2024-02-19 18:02:04 +0100

    reset + alert

--- assets/js/pendu/main.js
+++ assets/js/pendu/main.js
@@ -15,12 +15,15 @@ let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 
 let motSelectionne = "";
 
+let boutonValiderMot = document.getElementById("validerMot");
+let essayer = document.getElementById("essaye");
+
 let vie = 11; // Nombre de vie
 
 /**
  * Permet de réinitialiser les mots générés lorsque l'utilisateur change la difficulté
  */
-function resetMot() {
+function reset() {
     document.getElementById("generationMotFacile").innerText = "";
     document.getElementById("generationMotMoyen").innerText = "";
     document.getElementById("generationMotDifficile").innerText = "";
@@ -30,7 +33,7 @@ function resetMot() {
  * Si l'utilisateur clique sur le bouton "facile", un mot de la liste "motFacile" est généré
  */
 boutonFacile.addEventListener("click", function() {
-    resetMot();
+    reset();
     generationMotFacile = motFacile[Math.floor(Math.random() * motFacile.length)];
     let motATrouverFacile = generationMotFacile[0].toUpperCase() + " _ ".repeat(generationMotFacile.length - 2).toUpperCase() + generationMotFacile.toUpperCase()[generationMotFacile.length - 1];
     document.getElementById("generationMotFacile").innerText = motATrouverFacile;
@@ -41,7 +44,7 @@ boutonFacile.addEventListener("click", function() {
  * Si l'utilisateur clique sur le bouton "moyen", un mot de la liste "motMoyen" est généré
  */
 boutonMoyen.addEventListener("click", function() {
-    resetMot();
+    reset();
     generationMotMoyen = motMoyen[Math.floor(Math.random() * motMoyen.length)];
     let motATrouverMoyen = generationMotMoyen[0].toUpperCase() + " _ ".repeat(generationMotMoyen.length - 1).toUpperCase();
     document.getElementById("generationMotMoyen").innerText = motATrouverMoyen;
@@ -52,7 +55,7 @@ boutonMoyen.addEventListener("click", function() {
  * Si l'utilisateur clique sur le bouton "difficile", un mot de la liste "motDifficile" est généré
  */
 boutonDifficile.addEventListener("click", function() {
-    resetMot();
+    reset();
     generationMotDifficile = motDifficile[Math.floor(Math.random() * motDifficile.length)];
     let motATrouverDifficile = " _ ".repeat(generationMotDifficile.length - 1).toUpperCase();
     document.getElementById("generationMotDifficile").innerText = motATrouverDifficile;
@@ -61,6 +64,8 @@ boutonDifficile.addEventListener("click", function() {
 
 /**
  * Pour chaque lettre de l'alphabet, on ajoute un événement lorsqu'on clique sur le bouton correspondant
+ * Si l'utilisateur gagne, alors une alerte "Vous avez gagné !" s'affiche
+ * Sinon si l'utilisateur perd, une alerte "Vous avez perdu !" s'affiche
  */
 alphabet.forEach(function (lettre) {
     let bouton = document.getElementById(lettre);
@@ -75,6 +80,9 @@ alphabet.forEach(function (lettre) {
                 }
             }
             document.getElementById("mot").innerText = motAffiche;
+            if (!motAffiche.includes("_")) {
+                alert("Vous avez gagné !");
+            }
         } else {
             // Si la lettre n'est pas dans le mot, décrémente le nombre de vies et change l'image
             vie--;
@@ -82,16 +90,33 @@ alphabet.forEach(function (lettre) {
             let imgNum = 11 - vie;
             let imgNom = "pendu" + String(imgNum).padStart(2, '0') + ".png";
             document.getElementById("penduu").src = "../../assets/images/" + imgNom;
+            if (vie === -1) {
+                alert("Vous avez perdu !");
+            }
         }
     });
 });
 
-if (vie === 0) {
-    alert("Vous avez perdu !");
-}
 
-if (document.getElementById("mot").innerText === motSelectionne) {
-    alert("Vous avez gagné !");
-}
+boutonValiderMot.addEventListener("click", function() {
+    let motEssaye = essayer.value.toLowerCase();
+    if (motEssaye === motSelectionne) {
+        alert("Vous avez gagné !");
+    } else {
+        if (boutonDifficile.clicked) {
+            alert("Vous avez perdu !");
+        } else {
+            vie--;
+            document.getElementById("vieJoueur").innerText = vie;
+            let imgNum = 11 - vie;
+            let imgNom = "pendu" + String(imgNum).padStart(2, '0') + ".png";
+            document.getElementById("penduu").src = "../../assets/images/" + imgNom;
+            if (vie === -1) {
+                alert("Vous avez perdu !");
+            }
+        }
+    }
+    essayer.value = ""; // Clear the input field
+});
 
 document.getElementById("vieJoueur").innerText = vie;
