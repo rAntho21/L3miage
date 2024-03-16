b6666caa4ae8dd491d5d537b95a93657d16e13df
Carni-Lagarde Jayson <jayson.carni-lagarde@etu.univ-cotedazur.fr>
2024-03-16 11:48:09 +0100

    searchBar popup erreur en cours

---
+++ assets/js/searchBar.js
@@ -0,0 +1,26 @@
+// Liste de vos pages de jeu
+const pages = [
+    { name: 'Sudoku', url: 'src/jeux/sudoku.html' },
+    { name: 'Pendu', url: 'src/jeux/pendu.html' },
+    { name: 'Morpion', url: 'src/jeux/morpion.html' },
+    { name: 'Solitaire', url: 'src/jeux/solitaire.html' },
+    { name: 'Profil', url: 'src/navigation/profil.html' },
+    { name: 'Paramètre', url: 'src/navigation/parametres.html' },
+    { name: 'Jeux', url: 'src/navigation/listeJeux.html' },
+    { name: 'Boutique', url: 'src/navigation/boutique.html' }
+];
+
+document.getElementById('search-button').addEventListener('click', function() {
+    const searchMot = document.getElementById('search').value.toLowerCase();
+    let found = false;
+    for (let i = 0; i < pages.length; i++) {
+        if (pages[i].name.toLowerCase().includes(searchMot)) {
+            window.location.href = pages[i].url;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        alert('Aucun jeu ne correspond à votre recherche');
+    }
+});
