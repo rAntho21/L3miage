9ec8a42f5c6b7acced97cf1de118c96185bdd2d3
Rekimete <117949109+Rekimete@users.noreply.github.com>
2024-03-22 00:33:32 +0100

    Jeu jouable

--- assets/js/sudoku.js
+++ assets/js/sudoku.js
@@ -1,7 +1,22 @@
-// import joueur.js;
-// import grille.js;
+import Grille from "./GrilleSudoku.js";
 
-// Classe Sudoku
-class Sudoku {
+let grille;
 
+window.onload = init;
+
+function init() {
+    console.log("Page et ressources prêtes à l'emploi");
+
+    grille = new Grille();
+
+    // onclick des boutons
+    document.getElementById("btn-Facile").onclick = () => {
+        document.getElementById("loading").style.display = "block";
+        grille.choixDifficulte(1);};
+    document.getElementById("btn-Moyen").onclick = () => {
+        document.getElementById("loading").style.display = "block";
+        grille.choixDifficulte(2);};
+    document.getElementById("btn-Difficile").onclick = () => {
+        document.getElementById("loading").style.display = "block";
+        grille.choixDifficulte(3);};
 }
